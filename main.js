fc5e79abbae4f9c06193dedeb6d11c05ff8d7d8a
Justin Clark <justin@2906.co.uk>
2023-05-08 23:24:57 +0100

    v2

--- main.js
+++ main.js
@@ -4,32 +4,58 @@ var gameData = {
     gold: 0,
     biscuitMachines: 0,
     machineProd: 0,
-    nextMachineCost: 10,
     cats: 0,
+    biscuitsSold: 0,
     lastTick: Date.now()
   }
 
-  function update(id, content) { // a general update function
-    document.getElementById(id).innerHTML = content;
-  }
+if (typeof saveGame.biscuits !== "undefined") gameData.biscuits = saveGame.biscuits;
+if (typeof saveGame.biscuitsPerClick !== "undefined") gameData.biscuitsPerClick = saveGame.biscuitsPerClick;
+if (typeof saveGame.gold !== "undefined") gameData.gold = saveGame.gold;
+if (typeof saveGame.biscuitMachines !== "undefined") gameData.biscuitMachines = saveGame.biscuitMachines;
+if (typeof saveGame.machineProd !== "undefined") gameData.machineProd = saveGame.machineProd;
+if (typeof saveGame.cats !== "undefined") gameData.cats = saveGame.cats;
+if (typeof saveGame.biscuitsSold !== "undefined") gameData.biscuitsSold = saveGame.biscuitsSold;
+if (typeof saveGame.lastTick !== "undefined") gameData.lastTick = saveGame.lastTick;
 
-  function biscuitClick(number){ // manually make biscuit(s)
-    gameData.biscuits = gameData.biscuits + number;
+function tab(tab){
+    // hide all tabs and show the one selected
+    document.getElementById("biscuitMenu").style.display = "none"
+    document.getElementById("machinesMenu").style.display = "none"
+    document.getElementById("optionsMenu").style.display = "none"
+    document.getElementById(tab).style.display = "block"
+}
+// go to a tab for the first time
+tab("biscuitMenu")
+
+function update(id, content) { // a general update function
+    document.getElementById(id).innerHTML = content;
+}
+
+function biscuitClick(){ // manually make biscuit(s)
+    gameData.biscuits = gameData.biscuits + gameData.biscuitsPerClick;
     update("biscuits", format(gameData.biscuits, "sci"));
 };
 
-function buyBiscuitMachine(){
-    //var biscuitMachineCost = Math.floor(10 * Math.pow(1.1,biscuitMachines));     //works out the cost of this biscuit machine
-    if(gameData.gold >= gameData.nextMachineCost) {                                   //checks that the player can afford the cursor
-        gameData.biscuitMachines = gameData.biscuitMachines + 1;                                   //increases number of biscuitMachines
-    	gameData.gold = gameData.gold - gameData.nextMachineCost;                          //removes the biscuitss spent
+function biscuitMachineCosts(number){
+    var startnum = gameData.biscuitMachines
+    var thiscost = 0
+    for (i=startnum; i<(startnum+number); i ++) {
+        thiscost += Math.floor(10 * Math.pow(1.1,i));
+    }
+    return thiscost;
+}
+
+function buyBiscuitMachines(number){
+    var cost = biscuitMachineCosts(number)
+    if(gameData.gold >= cost) {                                    //checks that the player can afford the purchase
+        gameData.biscuitMachines = gameData.biscuitMachines + number;         //increases number of biscuitMachines
+    	gameData.gold = gameData.gold - cost;                                 //removes the gold spent
         update("biscuitMachines", format(gameData.biscuitMachines, "sci"));
-        update("gold", format(gameData.gold, "sci"));  //updates the number of cookies for the user
-    };
-    gameData.nextMachineCost = Math.floor(10 * Math.pow(1.1,gameData.biscuitMachines));       //works out the cost of the next cursor
-    update("biscuitMachineCost", format(gameData.nextMachineCost, "sci"));
-    gameData.machineProd = gameData.biscuitMachines;
-    update("machineProd", format(gameData.machineProd), "sci");
+        update("gold", format(gameData.gold, "sci"));               //updates the amount of gold for the user
+        gameData.machineProd = gameData.biscuitMachines * 1;
+        update("machineProd", format(gameData.machineProd), "sci");
+    }
 };
 
 var mainGameLoop = window.setInterval(function(){
@@ -44,6 +70,10 @@ var mainGameLoop = window.setInterval(function(){
     update("10pcBiscuits", format(pc10, "sci"));
     update("50pcBiscuits", format(pc50, "sci"));
     update("allBiscuits", format(gameData.biscuits, "sci"));
+    update("mc1", format(biscuitMachineCosts(1), "sci"));
+    update("mc10", format(biscuitMachineCosts(10), "sci"));
+    update("mc50", format(biscuitMachineCosts(50), "sci"));
+    update("mc100", format(biscuitMachineCosts(100), "sci"));
     update("cats", format (gameData.cats, "sci"));
 
 }, 1000);
@@ -52,7 +82,11 @@ var saveGameLoop = window.setInterval(function() {
     localStorage.setItem("catopocolypseSave", JSON.stringify(gameData))
   }, 15000)
 
-function loadGame(){
+function saveGame(){
+    localStorage.setItem("catopocolypseSave", JSON.stringify(gameData))
+}
+
+  function loadGame(){
     var savegame = JSON.parse(localStorage.getItem("catopocolypseSave"))
     if (savegame !== null) {
         gameData = savegame
@@ -73,6 +107,7 @@ function sellBiscuits(number) {
     if (gameData.biscuits >= selling) {
         gameData.biscuits = gameData.biscuits - selling;
         gameData.gold = gameData.gold + selling;
+        gameData.biscuitsSold += selling;
         update("biscuits", format(gameData.biscuits, "sci"));
         update("gold", format(gameData.gold, "sci"));
     }
